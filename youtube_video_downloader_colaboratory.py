a8a3d0e85f39a7962302488590a7ea14566bda27
Sourav <38432685+SouravApiDev@users.noreply.github.com>
2023-03-18 11:04:57 +0530

    Update youtube_video_downloader_colaboratory.py

--- youtube_video_downloader_colaboratory.py
+++ youtube_video_downloader_colaboratory.py
@@ -5,6 +5,7 @@ url = str(input("Youtube video url:- "))
 yt = YouTube(url)
 print(yt.title)
 Image(yt.thumbnail_url, width=320, height=180)
-video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+#video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 
-files.download(video)
+#files.download(video)
+#this time not working!
